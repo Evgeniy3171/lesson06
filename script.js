6ee384ce0503e457ece19773a08389b6b5fc3fa3
Evgeniy3171 <evgen915628@list.ru>
2021-04-08 21:04:52 +0300

    Сделал генерацию случайного числа и проверку его соответствия введенному числу

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let inputUser,
     numberRandom;
 inputUser = +prompt('Угадай число от 1 до 100');
-console.log(Math.floor(Math.random() * 101 + 0));
-let result = numberRandom === inputUser ? console.log('Поздравляю, Вы угадали!!!') : 
-numberRandom > inputUser ? console.log('Загаданное число больше') : 
+numberRandom = (Math.floor(Math.random() * 101 + 0));
+console.log(numberRandom);
+let result = (numberRandom === inputUser) ? console.log('Поздравляю, Вы угадали!!!') : 
+(numberRandom > inputUser) ? console.log('Загаданное число больше') : 
   console.log('Загаданное число меньше');
