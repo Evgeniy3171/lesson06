8ca5778076d3135324e5ea04d024ec60b96732b3
Evgeniy3171 <evgen915628@list.ru>
2021-04-09 20:24:40 +0300

    Выполнил домашк к уроку № 6

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-'use strict'
-let inputUser,
-    numberRandom;
-inputUser = +prompt('Угадай число от 1 до 100');
+'use strict';
+let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n) && n != "";
+};
+
+inputNumber();
+
+function inputNumber(){
+  let inputUser,
+      numberRandom,
+      result;
+
+inputUser = prompt('Угадай число от 1 до 100');
 numberRandom = (Math.floor(Math.random() * 101 + 0));
-console.log(numberRandom);
-let result = (numberRandom === inputUser) ? console.log('Поздравляю, Вы угадали!!!') : 
-(numberRandom > inputUser) ? console.log('Загаданное число больше') : 
-  console.log('Загаданное число меньше');
+console.log('Ваше число: ' + inputUser);
+
+if (inputUser === null){
+ alert('Игра окончена.');
+ return;
+} else {
+  result = (numberRandom === inputUser) ? alert('Поздравляю, Вы угадали!!!') :
+(numberRandom > inputUser) ? (alert('Загаданное число больше') + inputNumber()) :
+  (alert('Загаданное число меньше') + inputNumber());
+  return;
+}
+}
+
