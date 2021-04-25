7e3d31d93c03fac4de17b12cab322f2b72a1cafc
Evgeniy3171 <evgen915628@list.ru>
2021-04-25 19:51:15 +0300

    Усложненку по уроку 6 допилил.

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
 'use strict';
 
 function playNumber(){
-  let numberRandom = (Math.floor(Math.random() * 101 + 0));
+    let numberRandom = (Math.floor(Math.random() * 101 + 0));
+    let numberAttempts = 10;
 
-console.log(numberRandom);
+    console.log('Загаданное число: ', numberRandom);
 
-function inputNumber(){
-  let inputUser;
+    function inputNumber(){
+      numberAttempts--;
+      if (numberAttempts < 0){
+        if (confirm('Попытки закончились, хотите сыграть еще?')){
+          playNumber();
+        } else {
+          alert('До свидания');
+          return;
+        }
+      } else {          
+          let inputUser = prompt('Угадай число от 1 до 100');
+          console.log(isNaN(inputUser));
 
-  inputUser = prompt('Угадай число от 1 до 100');
-
-  console.log(isNaN(inputUser));
-
-  if (inputUser === null){
-    alert('Игра окончена.');
-    return;
-  } else if (inputUser.trim() === '' || inputUser === undefined || isNaN(inputUser)){// Убирает пробелы спереди и сзади
-    alert('Введите число от 0 до 100');
+          if (inputUser === null){
+              alert('Игра окончена.');
+              return;
+          } else if (inputUser.trim() === '' || inputUser === undefined || isNaN(inputUser)){// Убирает пробелы спереди и сзади
+              alert('Введите число от 0 до 100');
+              inputNumber();
+          } else if (+inputUser > numberRandom){
+              alert('Загаданное число меньше, осталось попыток: ' + numberAttempts);
+              inputNumber();
+          } else if (+inputUser < numberRandom){
+              alert('Загаданное число больше, осталось попыток: ' + numberAttempts);
+              inputNumber();
+          } else {
+              if (confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще?')){
+                inputNumber();
+              } else {
+                alert('До свидания');
+                return;
+              }
+            }  
+      }
+    }
     inputNumber();
-  } else if (+inputUser > numberRandom){
-      alert('Загаданное число меньше');
-      inputNumber();
-  } else if (+inputUser < numberRandom){
-      alert('Загаданное число больше');
-    inputNumber();
-  } else if (+inputUser === numberRandom){
-    alert('Поздравляю, Вы угадали!!!');
-    return;
-  }
-};
-
-inputNumber();
 }
 
 playNumber();
